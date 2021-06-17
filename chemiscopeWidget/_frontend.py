e7950ef1ddf9c5fe788b02b76640ddc3bba2b940
Jakub Lála <jakublala@gmail.com>
2021-06-17 18:29:47 +0200

    add missing python files

---
+++ chemiscopeWidget/_frontend.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# Copyright (c) Jakub Lala.
+# Distributed under the terms of the Modified BSD License.
+
+"""
+Information about the frontend package of the widgets.
+"""
+
+module_name = "chemiscope-widget"
+module_version = "^0.1.0"
---
+++ chemiscopeWidget/widget.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# Copyright (c) Jakub Lala.
+# Distributed under the terms of the Modified BSD License.
+
+"""
+TODO: Add module docstring
+"""
+import json
+
+from ipywidgets import DOMWidget, ValueWidget, register
+from traitlets import Unicode, Bool, validate, TraitError
+
+from ._frontend import module_name, module_version
+
+
+@register
+class Chemiscope(DOMWidget, ValueWidget):
+    _model_name = Unicode('ChemiscopeModel').tag(sync=True)
+    _model_module = Unicode(module_name).tag(sync=True)
+    _model_module_version = Unicode(module_version).tag(sync=True)
+
+    _view_name = Unicode('ChemiscopeView').tag(sync=True)
+    _view_module = Unicode(module_name).tag(sync=True)
+    _view_module_version = Unicode(module_version).tag(sync=True)
+
+    with open('../chemiscope.json') as f:
+        value = Unicode(json.dumps(json.load(f))).tag(sync=True)
